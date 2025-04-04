3835a1bdc7252bca079ea2d2777452bfc967587f
parvezalamk <parvezalamk@svaapta.com>
2025-04-04 16:53:49 +0530

    nine commit

--- src/index.php
+++ src/index.php
@@ -4,5 +4,6 @@ echo "This is CI/CD test.";
 echo "This is a test without a database.";
 echo "This is a test ";
 echo "This is deployment of Docker";
-
+echo "<br>";
+echo "CICD pipline is completed with docker deployment.";
 ?>

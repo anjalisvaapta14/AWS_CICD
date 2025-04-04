54be382239def79ee97a013b400b07b80bd740b6
parvezalamk <parvezalamk@svaapta.com>
2025-04-04 15:51:16 +0530

    fifth commit

--- src/index.php
+++ src/index.php
@@ -3,4 +3,6 @@ echo "Hello, Docker! This is a test without a database.";
 echo "This is CI/CD test.";
 echo "This is a test without a database.";
 echo "This is a test ";
+echo "This is deployment of Docker";
+
 ?>
